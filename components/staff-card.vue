e5cac4f2b7d5bb024216ba765e1524b23421ed4e
Phann Phearoen <phannphearin2@gmail.com>
2022-07-19 15:54:24 +0700

    add staffs page

---
+++ components/staff-card.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row class="staff-card my-2 mx-0 pa-0">
+    <v-col align-self="center" cols="4">
+      <v-img
+        :src="staff.img"
+        width="90%"
+        class="d-block mx-auto"
+        style="border-radius: 10px"
+      ></v-img>
+    </v-col>
+    <v-col align-self="center">
+      <div class="staff-name mb-1 white--text">「{{ staff.name }}」</div>
+      <v-divider dark></v-divider>
+      <v-chip class="my-2" small light label>Nickname is {{ staff.nick }}</v-chip>
+      <div
+        v-for="line in staff.intro"
+        :key="line"
+        class="intro"
+      >{{ line }}</div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: ['staff'],
+}
+</script>
+
+<style scoped>
+.staff-name {
+  font-size: 2rem;
+  font-weight: 500;
+}
+.intro {
+  font-size: 1.25rem;
+}
+</style>
---
+++ pages/staffs.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container class="demo-container">
+    <v-card dark outlined>
+      <v-card-title class="meka-title">STAFFS</v-card-title>
+      <v-divider dark></v-divider>
+      <v-card-text>
+        <staff-card
+          v-for="(staff, i) in staffs"
+          :key="i"
+          :staff="staff"
+        ></staff-card>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import staffCard from '../components/staff-card.vue'
+export default {
+  components: { staffCard },
+  data() {
+    return {
+      staffs: [
+        {
+          img: require(`~/assets/staffs/boss.jpeg`),
+          name: '職人',
+          nick: 'BOSS',
+          intro: [
+            'ビールは水。',
+            '海は焼肉。',
+            '休みは釣り。',
+            '　',
+            '職人。',
+          ],
+        },
+        {
+          img: require(`~/assets/staffs/staff1.jpeg`),
+          name: '誰よりも美しく',
+          nick: 'HIT',
+          intro: [
+            '誰よりも美しく取り付け',
+            '細部までカッコよく。',
+            '　',
+            'カワサキヲタク。',
+          ],
+        },
+        {
+          img: require(`~/assets/staffs/staff2.jpeg`),
+          name: 'Always Full Throttle.',
+          nick: 'OBA',
+          intro: [
+            '縁の下の腕利きニョーボ。',
+            '息子の母で店のハハ。',
+            '止まることを知らないSHARK RIDER.',
+            '　',
+            'タメコミストに嫌われがちの',
+            'ダンシャリスト。',
+          ],
+        },
+      ],
+    }
+  }
+}
+</script>
