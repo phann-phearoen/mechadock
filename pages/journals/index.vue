aa688ee030beaaef5193b323880e64529c095d6c
Phann Phearoen <phannphearin2@gmail.com>
2022-07-19 15:31:31 +0700

    add journals page

---
+++ pages/journals/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container class="demo-container">
+    <v-card outlined dark>
+      <v-card-title class="meka-title">工場日記</v-card-title>
+      <v-divider dark></v-divider>
+    </v-card>
+  </v-container>
+</template>
