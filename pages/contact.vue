9a9c5232d74a36c56140b31dc62a7d4cbfe186e7
Phann Phearoen <phannphearin2@gmail.com>
2022-07-19 16:03:15 +0700

    add contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,55 +1,55 @@
 <template>
-  <div>
-    <custom-title :title="'お問合せ'"></custom-title>
-    <v-container class="demo-container">
-      <v-row justify="center" class="ma-0 pa-0">
-        <v-col cols="10" sm="9">
-          <v-form>
-            <v-text-field 
-              outlined
-              requied
-              label="お名前"
-            ></v-text-field>
-            <v-text-field 
-              outlined
-              requied
-              label="電話番号"
-            ></v-text-field>
-            <v-text-field 
-              outlined
-              requied
-              label="メールアドレス"
-            ></v-text-field>
-            <v-textarea
-              label="お問合せ内容"
-              outlined
-              rows="8"
-            ></v-textarea>
-            <v-btn
-              :color="color"
-              min-width="200"
-              rounded
-              dark
-              class="d-block mx-auto mt-6"
-            >
-              ボタン
-            </v-btn>
-          </v-form>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-container class="demo-container">
+    <v-card dark outlined>
+      <v-card-title class="meka-title">お問合せ</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-row justify="center" class="ma-0 pa-0">
+          <v-col cols="12" sm="10">
+            <v-form>
+              <v-text-field 
+                outlined
+                requied
+                label="お名前"
+              ></v-text-field>
+              <v-text-field 
+                outlined
+                requied
+                label="ご住所"
+              ></v-text-field>
+              <v-text-field 
+                outlined
+                requied
+                label="電話番号"
+              ></v-text-field>
+              <v-text-field 
+                outlined
+                requied
+                label="メールアドレス"
+              ></v-text-field>
+              <v-textarea
+                label="お問合せ内容"
+                outlined
+                rows="8"
+              ></v-textarea>
+              <v-btn
+                :color="color"
+                min-width="200"
+                rounded
+                light
+                class="d-block mx-auto mt-6"
+              >
+                送信
+              </v-btn>
+            </v-form>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-import CustomTitle from '../components/recruit/recruit-title.vue'
-import { mapGetters } from 'vuex'
 export default {
-  components: {
-    CustomTitle,
-  },
-  computed: {
-    ...mapGetters('home', ['color']),
-  },
 }
 </script>
